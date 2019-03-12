50385ea3c567a26564032f55b2e7aea79b8f7e47
Manoj Immadi <manojqa5006@gmail.com>
2019-03-12 09:07:56 +0530

    Test

---
+++ 17thBatchUI/src/test/java/com/testcases/FirstTest.java
@@ -0,0 +1,23 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	
+	
+	@Test
+	public void tc1() {
+		System.out.println("First Test Case");
+	}
+	
+	@Test
+	public void tc2() {
+		System.out.println("Second Test Case");
+	}
+	
+	@Test
+	public void tc3() {
+		System.out.println("Third Test Case");
+	}
+}
---
+++ 17thBatchUI/src/test/java/com/testcases/SecondTest.java
@@ -0,0 +1,21 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test
+	public void tc4() {
+		System.out.println("Fourth Test Case");
+	}
+	
+	@Test
+	public void tc5() {
+		System.out.println("Fifth Test Case");
+	}
+	
+	@Test
+	public void tc6() {
+		System.out.println("Sixth Test Case");
+	}
+}
